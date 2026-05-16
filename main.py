73fe388e458e46c455ecfa3ef88857ed19e16710
smdijaj <smdijaj544@gmail.com>
2026-05-16 11:25:03 +0530

    Added main execution file

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.load_data import *
+from src.save_to_db import *
+from src.view_data import *
+from src.category_summary import *
+from src.chart_category import *
+from src.report_generator import *
+
+print("Expense Analyzer completed")
